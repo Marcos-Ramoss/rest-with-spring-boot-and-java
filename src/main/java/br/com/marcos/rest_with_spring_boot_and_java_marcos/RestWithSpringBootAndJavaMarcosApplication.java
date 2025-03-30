346846fd418a98993648d2a85ee2e2b14ad25d46
Marcos-Ramoss <mar.stark.99@gmail.com>
2025-03-30 15:31:59 -0400

    first commit

---
+++ src/main/java/br/com/marcos/rest_with_spring_boot_and_java_marcos/RestWithSpringBootAndJavaMarcosApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.rest_with_spring_boot_and_java_marcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaMarcosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcos/rest_with_spring_boot_and_java_marcos/RestWithSpringBootAndJavaMarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.rest_with_spring_boot_and_java_marcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaMarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
